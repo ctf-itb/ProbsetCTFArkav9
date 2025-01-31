550669780599b98d1cf26f5e344c5d7630a77692
abdulrafirh <abdulrafi623@gmail.com>
2025-01-31 13:36:37 +0700

    baby cry

---
+++ quals/cryptography/Enter The Arena/dist/chall.py
@@ -0,0 +1,24 @@
+from Crypto.Util.number import bytes_to_long, getPrime
+from Crypto.Random.random import randint
+
+FLAG = b"ARKAV{REDACTED}"
+
+p, q = getPrime(512), getPrime(512)
+n = p*q
+
+g, r1, r2 = randint(2, n), randint(2, n), randint(2, n)
+
+g1 = pow(g, r1 * (p - 1), n)
+g2 = pow(g, r2 * (q - 1), n)
+
+s1, s2 = randint(2, n), randint(2, n)
+
+m = bytes_to_long(FLAG)
+c1 = (m * pow(g1, s1, n)) % n
+c2 = (m * pow(g2, s2, n)) % n
+
+print(f"{n = }")
+print(f"{g1 = }")
+print(f"{g2 = }")
+print(f"{c1 = }")
+print(f"{c2 = }")
